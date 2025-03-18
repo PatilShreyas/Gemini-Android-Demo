2647c8c0ae718c157e18ab5aec997b3b0d0a5555
Shreyas Patil <shreyaspatilg@gmail.com>
2025-03-18 20:58:50 +0530

    feat: Enhance AssistantInterface with detailed function definitions and descriptions

--- app/src/main/java/dev/shreyaspatil/gemini/demo/aiservice/assistant/AssistantInterface.kt
+++ app/src/main/java/dev/shreyaspatil/gemini/demo/aiservice/assistant/AssistantInterface.kt
@@ -6,25 +6,53 @@ import com.google.ai.client.generativeai.type.Schema
 import com.google.ai.client.generativeai.type.defineFunction
 import org.json.JSONObject
 
+/**
+ * Assistant interface for performing the tasks in the device
+ */
 interface AssistantInterface {
+    /**
+     * Finds user details on GitHub
+     */
     suspend fun findOnGithub(username: String): String
+
+    /**
+     * Sends a text message to a contact
+     */
     suspend fun sendSms(contactName: String, message: String): String
+
+    /**
+     * Sends a WhatsApp message to a contact
+     */
     suspend fun sendWhatsAppMessage(contactName: String, message: String): String
+
+    /**
+     * Adds an item to the TODO list
+     */
     suspend fun addToList(item: String): String
+
+    /**
+     * Get all items from the TODO list
+     */
     suspend fun getAllItems(): String
 }
 
+/**
+ * Adapter for Assistant Interface to invoke functions or get the info about functions
+ */
 object AssistantInterfaceAdapter {
+    /**
+     * Get all functions available in the Assistant Interface
+     */
     fun getFunctions() = listOf(
         defineFunction(
-            name = "findGithubUser",
+            name = FunctionNames.FIND_GITHUB_USER,
             description = "Finds user details on GitHub",
             parameters = listOf(Schema.str("username", "GitHub username")),
             requiredParameters = listOf("username")
         ),
         defineFunction(
-            name = "sendTextSms",
-            description = "Finds user details on GitHub",
+            name = FunctionNames.SEND_TEXT_SMS,
+            description = "Sends a text message to a contact via SMS",
             parameters = listOf(
                 Schema.str("contactName", "Name of a contact to send a message"),
                 Schema.str("message", "Message content to send")
@@ -32,8 +60,8 @@ object AssistantInterfaceAdapter {
             requiredParameters = listOf("contactName", "message")
         ),
         defineFunction(
-            name = "sendWhatsAppMessage",
-            description = "Finds user details on GitHub",
+            name = FunctionNames.SEND_WHATSAPP_MESSAGE,
+            description = "Sends a message to a contact via WhatsApp",
             parameters = listOf(
                 Schema.str("contactName", "Name of a contact to send a message"),
                 Schema.str("message", "Message content to send")
@@ -41,7 +69,7 @@ object AssistantInterfaceAdapter {
             requiredParameters = listOf("contactName", "message")
         ),
         defineFunction(
-            name = "addToTODOs",
+            name = FunctionNames.ADD_TO_TODOS,
             description = "Adds a item to the TODO list",
             parameters = listOf(
                 Schema.str("item", "Item/message to be added in a TODO list"),
@@ -49,7 +77,7 @@ object AssistantInterfaceAdapter {
             requiredParameters = listOf("item")
         ),
         defineFunction(
-            name = "getAllTODOItems",
+            name = FunctionNames.GET_ALL_TODOS,
             description = "Get all items from the TODO list",
             parameters = listOf(
                 Schema.str("items", "Items of the todo list"),
@@ -57,7 +85,13 @@ object AssistantInterfaceAdapter {
         ),
     )
 
-    suspend fun invoke(functions: List<FunctionCallPart>, assistantInterface: AssistantInterface): List<FunctionResponsePart> {
+    /**
+     * Invoke functions with the given arguments
+     */
+    suspend fun invoke(
+        functions: List<FunctionCallPart>,
+        assistantInterface: AssistantInterface
+    ): List<FunctionResponsePart> {
         return functions.map { function ->
             invoke(function, assistantInterface)
         }
@@ -68,49 +102,44 @@ object AssistantInterfaceAdapter {
         assistantInterface: AssistantInterface
     ): FunctionResponsePart {
         return when (function.name) {
-            "findGithubUser" -> {
+            FunctionNames.FIND_GITHUB_USER -> {
                 val username = function.args["username"] as String
                 FunctionResponsePart(
                     function.name,
-                    ResponseTemplates.result(assistantInterface.findOnGithub(username))
+                    response(assistantInterface.findOnGithub(username))
                 )
             }
 
-            "sendTextSms" -> {
+            FunctionNames.SEND_TEXT_SMS -> {
                 val contactName = function.args["contactName"] as String
                 val message = function.args["message"] as String
                 FunctionResponsePart(
                     function.name,
-                    ResponseTemplates.result(assistantInterface.sendSms(contactName, message))
+                    response(assistantInterface.sendSms(contactName, message))
                 )
             }
 
-            "sendWhatsAppMessage" -> {
+            FunctionNames.SEND_WHATSAPP_MESSAGE -> {
                 val contactName = function.args["contactName"] as String
                 val message = function.args["message"] as String
                 FunctionResponsePart(
                     function.name,
-                    ResponseTemplates.result(
-                        assistantInterface.sendWhatsAppMessage(
-                            contactName,
-                            message
-                        )
-                    )
+                    response(assistantInterface.sendWhatsAppMessage(contactName, message))
                 )
             }
 
-            "addToTODOs" -> {
+            FunctionNames.ADD_TO_TODOS -> {
                 val item = function.args["item"] as String
                 FunctionResponsePart(
                     function.name,
-                    ResponseTemplates.result(assistantInterface.addToList(item))
+                    response(assistantInterface.addToList(item))
                 )
             }
 
-            "getAllTODOItems" -> {
+            FunctionNames.GET_ALL_TODOS -> {
                 FunctionResponsePart(
                     function.name,
-                    ResponseTemplates.result(assistantInterface.getAllItems())
+                    response(assistantInterface.getAllItems())
                 )
             }
 
@@ -118,7 +147,13 @@ object AssistantInterfaceAdapter {
         }
     }
 
-    object ResponseTemplates {
-        fun result(status: String) = JSONObject(mapOf("result" to status))
+    private fun response(status: String) = JSONObject(mapOf("result" to status))
+
+    object FunctionNames {
+        const val FIND_GITHUB_USER = "findGithubUser"
+        const val SEND_TEXT_SMS = "sendTextSms"
+        const val SEND_WHATSAPP_MESSAGE = "sendWhatsAppMessage"
+        const val ADD_TO_TODOS = "addToTODOs"
+        const val GET_ALL_TODOS = "getAllTODOItems"
     }
 }
